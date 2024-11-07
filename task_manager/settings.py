0e0703a6cae505b45602ec59a20829d6c9a614ff
unknown <aadhavraj0406@gmail.com>
2024-11-07 18:29:40 +0530

    settings modification commit

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -153,10 +153,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "task.User"
-FERNET_KEY=b'NQwbOC-aqiZUW8Fmm563eNUtyxHdftvV1HSU0fm29Wc='
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT="587"
-EMAIL_HOST_USER="aadhavraj0406@gmail.com"
-EMAIL_HOST_PASSWORD="kglw segi dlax kweh"
-EMAIL_USE_TLS=True
+FERNET_KEY=#EMAIL_BACKEND = EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=EMAIL_HOST_USER=EMAIL_HOST_PASSWORD=EMAIL_USE_TLS=
